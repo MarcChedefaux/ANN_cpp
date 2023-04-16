a2dc085444cbc7210115c8976a8ef1c35873b3bb
MarcChedefaux <chedefaux.marc@gmail.com>
2023-04-16 11:08:42 +0200

    Addition of the activation function derivative : useful for learning

--- src/activationFunction.cpp
+++ src/activationFunction.cpp
@@ -1,12 +1,12 @@
 #include "activationFunction.hpp"
 
-activation identity = {Identity, 0};
-activation binarystep = {BinaryStep, 1};
-activation sigmoid = {Sigmoid, 2};
-activation tanhyperbolic = {TanHyperbolic, 3};
-activation relu = {ReLu, 4};
-activation leakyrelu = {LeakyReLu, 5};
-activation gaussian = {Gaussian, 6};
+activation identity = {Identity, IdentityPrime, 0};
+activation binarystep = {BinaryStep, BinaryStepPrime, 1};
+activation sigmoid = {Sigmoid, SigmoidPrime, 2};
+activation tanhyperbolic = {TanHyperbolic, TanHyperbolicPrime, 3};
+activation relu = {ReLu, ReLuPrime, 4};
+activation leakyrelu = {LeakyReLu, LeakyReLuPrime, 5};
+activation gaussian = {Gaussian, GaussianPrime, 6};
 
 std::vector<activation> ArrayOfActivation = {identity, binarystep, sigmoid, tanhyperbolic, relu, leakyrelu, gaussian};
 
@@ -14,8 +14,16 @@ double Identity(double x) {
     return x;
 }
 
+double IdentityPrime(double x) {
+    return 1;
+}
+
 double BinaryStep(double x){
     return (x>=0?1:0);
+}
+
+double BinaryStepPrime(double x) {
+    return 0;
 }
 
 
@@ -23,22 +31,41 @@ double Sigmoid(double x){
     return (double)(1/(1+exp(-x)));
 }
 
+double SigmoidPrime(double x) {
+    double s = Sigmoid(x);
+    return s * (1-s);
+}
+
 
 double TanHyperbolic(double x){
     return tanh(x);
 }
 
+double TanHyperbolicPrime(double x){
+    double g = TanHyperbolic(x);
+    return 1 - g*g;
+}
 
 double ReLu(double x){
     return fmax(0,x);
 }
 
+double ReLuPrime(double x){
+    return (x>=0?1:0);
+}
 
 double LeakyReLu(double x){
     return (x>=0?x:0.1*x);
 }
 
+double LeakyReLuPrime(double x) {
+    return (x>=0?1:0.01);
+}
 
 double Gaussian(double x){
     return exp(-(x*x)); 
+}
+
+double GaussianPrime(double x) {
+    return -2 * x * Gaussian(x);
 }
--- src/activationFunction.hpp
+++ src/activationFunction.hpp
@@ -16,6 +16,7 @@ typedef double (*activationFunction)(double args);
  */
 typedef struct {
     activationFunction function;
+    activationFunction derivative;
     int index;
 } activation;
 
@@ -29,12 +30,28 @@ typedef struct {
 double Identity(double x);
 
 /**
+ * @brief Identity derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double IdentityPrime(double x);
+
+/**
  * @brief Binary step activation function
  * 
  * @param x 
  * @return double 
  */
 double BinaryStep(double x);
+
+/**
+ * @brief Binary Step derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double BinaryStepPrime(double x);
 
 /**
  * @brief Sigmoid activation function
@@ -45,12 +62,28 @@ double BinaryStep(double x);
 double Sigmoid(double x);
 
 /**
+ * @brief Sigmoid derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double SigmoidPrime(double x);
+
+/**
  * @brief TanH activation function
  * 
  * @param x 
  * @return double 
  */
 double TanHyperbolic(double x);
+
+/**
+ * @brief TanH derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double TanHyperbolicPrime(double x);
 
 /**
  * @brief ReLU activation function
@@ -61,6 +94,14 @@ double TanHyperbolic(double x);
 double ReLu(double x);
 
 /**
+ * @brief ReLu derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double ReLuPrime(double x);
+
+/**
  * @brief Leaky ReLU activation function
  * 
  * @param x 
@@ -69,12 +110,28 @@ double ReLu(double x);
 double LeakyReLu(double x);
 
 /**
+ * @brief LeakyReLu derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double LeakyReLuPrime(double x);
+
+/**
  * @brief Gaussian activation function
  * 
  * @param x 
  * @return double 
  */
 double Gaussian(double x);
+
+/**
+ * @brief Gaussian derivative
+ * 
+ * @param x 
+ * @return double 
+ */
+double GaussianPrime(double x);
 
 extern activation identity;
 extern activation binarystep;
